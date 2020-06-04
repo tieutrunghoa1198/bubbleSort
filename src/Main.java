5d56beb2698009d8d6ab1158253c0efb5cef231a
Hoà Tiêu <tieutrunghoa1198@gmail.com>
2020-06-04 09:38:27 +0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author songm
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean isSwapped;
+        for (int i = 0; i < n; i++) {
+            isSwapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if (isSwapped == false) {
+                break;
+            }
+        }
+    }
+
+    void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Main main = new Main();
+        Random random = new Random();
+
+        System.out.print("Enter array's length: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(size);
+        }
+        System.out.print("Array before sorted: ");
+        main.display(arr);
+
+        main.bubbleSort(arr);
+
+        System.out.print("Array after sorted: ");
+        main.display(arr);
+    }
+
+}
